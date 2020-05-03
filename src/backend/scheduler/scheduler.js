35d531884134f420f62ca2699180ca360f8066f0
leo.karoubi <leokaroubi@gmail.com>
2020-05-03 10:16:51 +0200

    scheduler cleanup

--- src/backend/scheduler/scheduler.js
+++ src/backend/scheduler/scheduler.js
@@ -1,64 +1,16 @@
 "use strict";
 
 /* [ENVIRRONNEMENT VARIABLE] */
-const DDB_ID="ParadigmShift-context" 
+const DDB_ID="paradigmshift-context" 
 /* [ENVIRRONNEMENT VARIABLE] */
 
 const AWS = require('aws-sdk');
 
-function fleet_start(id)
-{
-    return new Promise((resolve, reject) => {
-        const params = {
-            Name: id
-        };
-        appsteam.startFleet(params, function(err, data) {
-            if (err)
-                return reject(err);
-            else
-                return resolve("Success")
-        });
-    });
-}
-
-function fleet_stop(id)
-{
-    return new Promise((resolve, reject) => {
-        const params = {
-            Name: id
-        };
-        appsteam.stopFleet(params, function(err, data) {
-            if (err)
-                return reject(err);
-            else
-                return resolve("Success")
-        });
-    });
-}
 exports.handler = async (event, context, callback) =>
 {
-    let event_array;
-    var promised;
-    var response = {
-        statusCode: 500,
-        body: "Bad data format",
-        headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin':'*'}
-    };
     try {
-        event_array = JSON.parse(event.body);
-        const fleetid = event_array.fleetid;
-        const action = event_array.action;
+        /* Get the database timer */
     } catch (err) {
-        return callback(null, response);
+        return callback(e);
     }
-    if (action === "START")
-        promised = await fleet_start(fleetid);
-    else if (action === "STOP")
-        promised = await fleet_stop(fleetid);
-    else
-        return callback(null, response);
-    response.body = promised;
-    if (promised == "Success")
-        response.statusCode = 200;
-    return callback(null, response);
 };
