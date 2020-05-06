1ed20b87f2b0aff77e436afcd984ba84ede4555e
leo.karoubi <leokaroubi@gmail.com>
2020-05-06 11:07:35 +0200

    start the deploy script

---
+++ deploy/deploy.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+
+# Variables required : 
+# region
+
+echo $@
+
+##
+## Entry checkup
+##
+
+if [ $# -ne 1 ] || [ $1 == "--help" ]; then
+    echo "$0 \$region"
+    exit 0
+fi
+
+project="paradigmshift"
+region=$1
+bucket=$project-sambuild
+
+##
+## Environnement setup
+##
+
+BUILD="build"
+
+mkdir -p $BUILD
+
+##
+## Set the script controlflow
+##
+
+function CLEANUP()
+{
+    rm -rf $BUILD
+    rm -rf .aws-sam
+}
+
+function RAISE()
+{
+    echo "Process terminated, fatal error"
+    CLEANUP
+    exit 0
+}
+
+set -e
+
+trap RAISE EXIT
+
+##
+## Start deploying
+##
+
+echo "-------- Create SAM bucket --------"
+
+aws s3api create-bucket --bucket $bucket --region $region --create-bucket-configuration LocationConstraint=$region
+
+echo "-------- Install dependencies --------"
+
+npm install ../src/backend --prefix ../src/backend
+
+echo "-------- Deploy Resources --------"
+
+sam build 
+
+sam package \
+    --s3-bucket $bucket \
+    --output-template-file build/package.yml
+
+sam deploy \
+    --template-file build/package.yml \
+    --stack-name $project \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --region $region \
+    --tags Project=$project \
+    --parameter-overrides \
+        Project=$project \
+
+CLEANUP
+
+echo "We are done !"
+
+trap - EXIT
