3eaf9bf61cb84bb1f318285d5c5f612611700092
leo.karoubi <leokaroubi@gmail.com>
2020-05-06 10:51:30 +0200

    start scheduler & update package.json

--- src/backend/scheduler/scheduler.js
+++ src/backend/scheduler/scheduler.js
@@ -6,11 +6,23 @@ const DDB_ID="paradigmshift-context"
 
 const AWS = require('aws-sdk');
 
+const cron = require('cron-parser');
+
+function parse_ISO8601(asStr)
+{
+    const asMs = Date.parse(asStr);
+    if (asMs === NaN)
+        return (Nan);
+    return (asMs * 1000);
+}
+
+// function schedulingEval(current, )
+
 exports.handler = async (event, context, callback) =>
 {
     try {
         /* Get the database timer */
     } catch (err) {
-        return callback(e);
+        return callback(err);
     }
 };
