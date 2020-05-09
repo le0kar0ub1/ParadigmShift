193d51f4b7dbadab95b07395e3c7cadcef2adcc8
leo.karoubi <leokaroubi@gmail.com>
2020-05-09 23:38:13 +0200

    config script & output API endpoint

--- deploy/config.sh
+++ deploy/config.sh
@@ -18,7 +18,7 @@ function CLEANUP()
 function RAISE()
 {
     CLEANUP
-    printf "Process terminated, fatal error"
+    echo "Process terminated, fatal error"
     exit 0
 }
 
@@ -27,6 +27,17 @@ set -e
 trap RAISE EXIT
 
 # First we need the APIs endpoints 
-printf "const API_CONTEXTGET_ENDPOINT=" > $TARGET
+echo -n "const API_BASEURL=" > $TARGET
+echo -e "\"$theapiurl\"" >> $TARGET
+
+echo -e "const API_CONTEXTGET_ENDPOINT=\"$theapiurl\"" >> $TARGET
+echo -e "const API_CONTEXTREGISTER_ENDPOINT=\"$theapiurl\"" >> $TARGET
+
+echo -en "
+const resourceref = [
+    \"ec2::instance\",
+    \"rds::instance\",
+    \"appstream::fleet\"
+];" >> $TARGET
 
 trap - EXIT
