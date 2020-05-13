41f99b3deb64a62f98ed68f49faf77d64dea7f26
leo.karoubi <leokaroubi@gmail.com>
2020-05-13 13:16:06 +0200

    add tag front description

--- src/frontend/static/main.js
+++ src/frontend/static/main.js
@@ -1,4 +1,5 @@
 var contextlist;
+var tagList;
 
 function backend_request_context(contextid, type)
 {
@@ -14,30 +15,67 @@ function backend_request_context(contextid, type)
     });
 }
 
+function backend_request_tag(tagKey, type)
+{
+    return new Promise((resolve, reject) => {
+        axios.post(API_CONTEXTGET_ENDPOINT, {
+            data: {
+                type: type,
+                tagKey: tagKey
+            }
+        }).then(function (response) {
+            return resolve (response.data.body);
+        })
+    });
+}
+
 function contextDescription()
 {
     try {
         const select = document.getElementById("contextSelector");
         const data = contextlist[select.selectedIndex];
-        var context = document.getElementById("desc-context");
-        var desc = document.getElementById("desc-desc");
-        var rulestart = document.getElementById("desc-rule-start");
-        var rulestop = document.getElementById("desc-rule-stop");
-        var issched = document.getElementById("desc-issched");
+        var context = document.getElementById("desc-context-context");
+        var desc = document.getElementById("desc-context-desc");
+        var rulestart = document.getElementById("desc-context-rule-start");
+        var rulestop = document.getElementById("desc-context-rule-stop");
+        var issched = document.getElementById("desc-context-issched");
         context.textContent = data.contextID;
         desc.textContent = data.contextDesc;
         rulestart.textContent = data.schedulingRuleStart;
         rulestop.textContent = data.schedulingRuleStop;
         issched.textContent = data.isScheduled == true ? "True" : "False";
-        setPowerState(data.powerState);
+        setPowerState(data.powerState, "contextPowerState");
     } catch (err) {
         console.log("An unexpected error occured: " + err);
     }
 }
 
-function setPowerState(bool)
+function tagDescription()
 {
-    var state = document.getElementById("powerState");
+    try {
+        const select = document.getElementById("tagSelector");
+        const data = tagList[select.selectedIndex];
+        var tagkey = document.getElementById("desc-tag-key");
+        var tagvalues = document.getElementById("desc-tag-values");
+        var desc = document.getElementById("desc-tag-desc");
+        var rulestart = document.getElementById("desc-tag-rule-start");
+        var rulestop = document.getElementById("desc-tag-rule-stop");
+        var issched = document.getElementById("desc-tag-issched");
+        tagkey.textContent = data.tagKey;
+        tagvalues.textContent = data.tagValues;
+        desc.textContent = data.tagDesc;
+        rulestart.textContent = data.schedulingRuleStart;
+        rulestop.textContent = data.schedulingRuleStop;
+        issched.textContent = data.isScheduled == true ? "True" : "False";
+        // setPowerState(data.powerState, "contextPowerState");
+    } catch (err) {
+        console.log("An unexpected error occured: " + err);
+    }
+}
+
+function setPowerState(bool, id)
+{
+    var state = document.getElementById(id);
 
     if (bool == true)
         state.textContent = "State: RUNNING";
@@ -48,21 +86,33 @@ function setPowerState(bool)
 async function preload()
 {
     return;
-    const rawdata = await backend_request_context("restricted-all", "context");
+    const rawdatacontext = await backend_request_context("restricted-all", "context");
+    const rawdatatag = await backend_request_tag("restricted-all", "tag");
 
     try {
-        contextlist = JSON.parse(rawdata);
+        contextlist = JSON.parse(rawdatacontext);
+        tagList = JSON.parse(rawdatatag);
     } catch (err) {
         console.log("Database access failed: " + err);
         return;
     }
-    var select = document.getElementById("contextSelector");
+    var contextSelect = document.getElementById("contextSelector");
     for(let i = 0; contextlist[i]; i++)
     {
         var elem = document.createElement("option");
         elem.textContent = contextlist[i].contextID;
         elem.value = i;
-        select.appendChild(elem);
+        contextSelect.appendChild(elem);
     }
+    var tagSelect = document.getElementById("tagSelector");
+    for(let i = 0; tagList[i]; i++)
+    {
+        var elem = document.createElement("option");
+        elem.textContent = tagList[i].tagKey;
+        elem.value = i;
+        tagSelect.appendChild(elem);
+    }
+    // flushing now
     contextDescription();
+    tagDescription();
 }
